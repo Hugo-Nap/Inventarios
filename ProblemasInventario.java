c0367bc8e9342e701f419770552b9e303362adf5
Hugo Fdz <hugobillclave@gmail.com>
2025-11-09 12:11:10 -0600

    Actualizacion_2

--- ProblemasInventario.java
+++ ProblemasInventario.java
@@ -15,12 +15,25 @@ public class ProblemasInventario {
         double L = reader.nextDouble();
 
         //llamado de metodos
-        System.out.println("Cantidad de pedido" + CantidadPedido(k, D, h));
+        System.out.println("y* = " + cantidadPedido(k, D, h));
+        System.out.println("t0 = " + tCero(D, cantidadPedido(k, D, h)));
+        System.out.println("Le = " + Le(L,valorN(L,tCero(D, cantidadPedido(k, D, h))),tCero(D,cantidadPedido(k, D, h))));
     }
     //metodos para cada una de las formulas
     //metodo del valor 'y' 
-    public static double CantidadPedido(double k, double d, double h){
+    public static double cantidadPedido(double k, double d, double h){
         return Math.sqrt((2*k*d)/h);
     }
     //metodo del valor 't0' 
+    public static double tCero(double d, double y){
+        return y/d;
+    }
+    //metodo del valor 'n'
+    public static double valorN(double L,double t0){
+        return L/t0;
+    }
+    //metodo del valor 'Le'
+    public static double Le(double L, double n, double t0){
+        return(8-(2*t0));
+    }
 }
